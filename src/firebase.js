541675d9ab5b5c3a7876fa78f68963fefcfb1c86
John Gachara <johngachara29@gmail.com>
2024-09-19 12:21:28 +0300

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,6 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
     authDomain: import.meta.env.VITE_AUTHDOMAIN,
-    databaseURL: import.meta.env.VITE_DATABASEURL,
     projectId: import.meta.env.VITE_PROJECT_ID,
     storageBucket:import.meta.env.VITE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
